f6937431a4094064db969b187f299eeb35b4f417
Himanshu Sahni <sahnihimanshu97@gmail.com>
2018-08-17 09:19:03 +0530

    Translation fade image goes down when we tap on it
    Signed-off-by: Himanshu Sahni <sahnihimanshu97@gmail.com>

--- 04Images,media,sounds/03FadingAnimations/app/src/main/java/com/himanshu/a03fadinganimations/MainActivity.java
+++ 04Images,media,sounds/03FadingAnimations/app/src/main/java/com/himanshu/a03fadinganimations/MainActivity.java
@@ -8,16 +8,13 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     public  void fade(View view){
-        // to fade the image when we tap on it
-        ImageView cartoon1 = findViewById(R.id.cartoon1);
 
-        // what to change and for how long for the change to happen
-        // alpha is no from 0 to 1 how transparent the image is , 0 is invisible ,1 is completeley visible
+        ImageView cartoon1 = findViewById(R.id.cartoon1);
 
         ImageView cartoon2 = findViewById(R.id.cartoon2);
 
-        cartoon1.animate().alpha(0f).setDuration(2000 ); // f is there to make it float
-        cartoon2.animate().alpha(1f).setDuration(2000);
+        cartoon1.animate().translationYBy(1000f).setDuration(2000 ); //y is the vertical axis
+
 
     }
     @Override
