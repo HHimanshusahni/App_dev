ee561799ad7b6b5d9680279ff6321e5e54713056
Himanshu Sahni <sahnihimanshu97@gmail.com>
2018-08-23 15:34:04 +0530

    Adding seekbar to go particular time in video
    Signed-off-by: Himanshu Sahni <sahnihimanshu97@gmail.com>

--- 04Images,media,sounds/06SoundDemo/app/src/main/java/com/himanshu/a06sounddemo/MainActivity.java
+++ 04Images,media,sounds/06SoundDemo/app/src/main/java/com/himanshu/a06sounddemo/MainActivity.java
@@ -10,11 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer ;
 
     AudioManager audioManager;
+
+
 
     public void playAudio(View view){
         mediaPlayer.start();
@@ -54,6 +59,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+            }
+        });
+
+        final SeekBar scrubber = findViewById(R.id.scrubber);
+       scrubber.setMax(mediaPlayer.getDuration());
+
+       new Timer().scheduleAtFixedRate(new TimerTask() {
+           @Override
+           public void run() {
+            scrubber.setProgress(mediaPlayer.getCurrentPosition()   );
+           }
+       },0,100);// run the code immediately and after every  10th second
+
+        scrubber.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.pause();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.start();
             }
         });
 
