a6890d3e6ebb2f2b28af70f2074aa46751a41660
Himanshu Sahni <sahnihimanshu97@gmail.com>
2018-08-29 19:49:32 +0530

    Making the textview invisible
    Signed-off-by: Himanshu Sahni <sahnihimanshu97@gmail.com>

--- 05AdvancedAnroidFeatures/05ShowingAndHidingUIElements/app/src/main/java/com/himanshu/a05showingandhidinguielements/MainActivity.java
+++ 05AdvancedAnroidFeatures/05ShowingAndHidingUIElements/app/src/main/java/com/himanshu/a05showingandhidinguielements/MainActivity.java
@@ -2,6 +2,9 @@ package com.himanshu.a05showingandhidinguielements;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnHide = (Button)findViewById(R.id.btnHide);
+        Button btnShow = (Button)findViewById(R.id.btnShow);
+        final TextView textView = (TextView)findViewById(R.id.tvHELLO);
+        btnHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setAlpha(0.0f);
+            }
+        });
+
+        btnShow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+            textView.setAlpha(1.0f);
+
+            }
+        });
     }
 }
