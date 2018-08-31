c00b3a60e487a6f0002f854c04bb12667d4bbe44
Himanshu Sahni <sahnihimanshu97@gmail.com>
2018-08-31 22:29:06 +0530

    3 methods to manipulate the string
    Signed-off-by: Himanshu Sahni <sahnihimanshu97@gmail.com>

--- 05AdvancedAnroidFeatures/10 AdvancedStringManipulation/src/MyClass.java
+++ 05AdvancedAnroidFeatures/10 AdvancedStringManipulation/src/MyClass.java
@@ -1,19 +1,61 @@
 import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 
 public class MyClass {
 
-	// Split single big string into no of smaller strings
+
 	
 	public static void main(String args[]) {
-		
-		String myString = "Ram x Rohan x Mohit";
-//		String[] splitString = myString.split("x");
-//		Output[Ram ,  Rohan ,  Mohit]
-		
-		String[] splitString = myString.split(" x ");
-			
-		System.out.println(Arrays.toString(splitString));
 
+				// Method 1
+		
+/*//		----- Spliting another string-----
+		String river = "Mississippi";
+		String [] splitriver = river.split("s");
+		
+		System.out.println(Arrays.toString(splitriver));
+				// o/p [Mi, , i, , ippi]
+*/		
+		
+		
+		
+		// 			Method 2
+		
+/*//		----------Using substring function----------
+		String river = "Mississippi";
+//		String riverPart = river.substring(2,5); // o/p       ssi
+		
+		String riverPart = river.substring(4,8); //o/p       issi
+		
+		System.out.println(riverPart);
+*/		
+		
+		
+		
+		
+		//  		Method 3
+	   //     JAVA REGEX  : Regular expressions
+		//  we create a pattern in this method
+		
+//		String river = "Mississippi";   // o/p ssissip
+		
+		String river = "MississippiMississippi"; 
+		
+		Pattern p  = Pattern.compile("Mi(.*?)pi");
+		
+		Matcher m = p.matcher(river);
+		
+		while(m.find()) {
+			System.out.println(m.group(1));
+		}
+		
+		
+		
+		
+		
+		
 		
 		
 		
